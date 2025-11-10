e18653d6f6975898079e6145deb51a69842abec2
Emirhan Ülgen <emirhanulgen34@gmail.com>
2025-11-10 21:46:35 +0000

    updates

---
+++ app/src/main/java/com/emoulgen/tvapp/ChannelItemActivity.kt
@@ -0,0 +1,15 @@
+package com.emoulgen.tvapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ChannelItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_channel_item)
+    }
+}
--- app/src/main/java/com/emoulgen/tvapp/adapter/ChannelItemAdapter.kt
+++ app/src/main/java/com/emoulgen/tvapp/adapter/ChannelItemAdapter.kt
@@ -1,9 +1,11 @@
 package com.emoulgen.tvapp.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.emoulgen.tvapp.ChannelItemActivity
 import com.emoulgen.tvapp.databinding.ChannelItemBinding
 
 class ChannelItemAdapter(val context: Context, val list: List<String>): RecyclerView.Adapter<ChannelItemAdapter.ChannelItemViewHolder>() {
@@ -23,7 +25,8 @@ class ChannelItemAdapter(val context: Context, val list: List<String>): Recycler
             linkName.text = "Link ${list[position]}"
 
             root.setOnClickListener {
-
+                val intent = Intent(context, ChannelItemActivity::class.java)
+                context.startActivity(intent)
             }
         }
     }
