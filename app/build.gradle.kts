a0802c959f417245ae2f47be732fd6ad2c89d82c
Emirhan Ülgen <emirhanulgen34@gmail.com>
2025-11-17 16:54:49 +0000

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.leanback)
+    implementation(libs.androidx.leanback.paging)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
